edd482f8b47e23c1f399a66d72f26e743999ba75
alexbypa <alexbypa@gmail.com>
2026-09-14 15:13:57 +0200

    fix NpgsqlCommand injection postgresql

--- FlowScheduler.Infrastructure/Database/PostgreSqlConnectionFactory.cs
+++ FlowScheduler.Infrastructure/Database/PostgreSqlConnectionFactory.cs
@@ -1,4 +1,4 @@
-﻿using FlowScheduler.Core.Interfaces.Data;
+using FlowScheduler.Core.Interfaces.Data;
 using Npgsql;
 using System;
 using System.Collections.Generic;
@@ -14,9 +14,13 @@ public class PostgreSqlConnectionFactory : IDbConnectionFactory {
     public bool SupportsStoredProcedures => true;
     public void AddParameter(DbCommand command, string name, object value) => command.Parameters.Add(new NpgsqlParameter(name, value));
     public DbCommand CreateCommand(string commandText, DbConnection connection) {
-        var cmd = new NpgsqlCommand($"SELECT * FROM {commandText}()", (NpgsqlConnection)connection);
-        cmd.CommandType = CommandType.Text;
+        var cmd = new NpgsqlCommand(commandText, (NpgsqlConnection)connection);
+        cmd.CommandType = CommandType.StoredProcedure;
         return cmd;
     }
-    public DbConnection CreateConnection(string connectionString) => new NpgsqlConnection(connectionString);
+    public DbConnection CreateConnection(string connectionString)
+    {
+        var builder = new NpgsqlConnectionStringBuilder(connectionString);
+        return new NpgsqlConnection(builder.ConnectionString);
+    }
 }
